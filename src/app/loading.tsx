a034b48da51904f117b696018d1ec726d17b6dc5
alx <aleksandar.zedi@gmail.com>
2024-02-01 17:30:57 +0100

    Updated Loading & 404 Page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,12 @@
+import { LoadingOutlined } from "@ant-design/icons";
+import FlexWrapper from "./_components/elements/flex-wrapper";
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div>Loading ....</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <FlexWrapper justifyContent="center" alignItems="center">
+        <LoadingOutlined style={{ fontSize: "40px" }} />
+      </FlexWrapper>
+    </div>
+  );
 }
